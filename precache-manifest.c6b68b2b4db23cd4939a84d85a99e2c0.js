54b6017dca7e334d9ef843ae2fd3564794e7559a
unknown <Humans@Murph.E>
2020-01-16 18:09:53 -0500

    Updates

---
+++ precache-manifest.c6b68b2b4db23cd4939a84d85a99e2c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2387836d815fea993c9bcb0a088deb0b",
+    "url": "/BrawlBracket/public/index.html"
+  },
+  {
+    "revision": "e10cac6b2d1082635bdb",
+    "url": "/BrawlBracket/public/static/css/main.2e3f5ad7.chunk.css"
+  },
+  {
+    "revision": "4a6ad1cf8c5682c7e065",
+    "url": "/BrawlBracket/public/static/js/2.2e041677.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/BrawlBracket/public/static/js/2.2e041677.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e10cac6b2d1082635bdb",
+    "url": "/BrawlBracket/public/static/js/main.59a4a054.chunk.js"
+  },
+  {
+    "revision": "b8eb28388796fe772543",
+    "url": "/BrawlBracket/public/static/js/runtime-main.6cd5593c.js"
+  },
+  {
+    "revision": "6f17185a7cb7e8002dfaf13f82b20616",
+    "url": "/BrawlBracket/public/static/media/The_Brawl.6f17185a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BrawlBracket/precache-manifest.138eb6190fbf42752c3dbfd94b89efd7.js"
+  "/BrawlBracket/public/precache-manifest.c6b68b2b4db23cd4939a84d85a99e2c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BrawlBracket/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BrawlBracket/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
